7d06b0e8b96b22b188e2b14310f58568bf5249a3
Sidath Weerasinghe <sidath@wso2telco.com>
2017-02-22 20:40:37 +0530

    Update README.md and DB scripts

--- service/dbscripts/baseline.sql
+++ service/dbscripts/baseline.sql
@@ -114,4 +114,20 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-06-23 10:18:38
+
+DROP TABLE IF EXISTS `tstemail`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tstemail` (
+  `idtstemail` int(11) NOT NULL AUTO_INCREMENT,
+  `tsxwhodid` int(11) NOT NULL,
+  `tstmailaddr` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`idtstemail`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+ALTER TABLE `tsxwho` ADD ( `reattmptcount` int(3) DEFAULT NULL,
+  `retrymax` int(3) DEFAULT NULL,
+ `retrydelay` int(11) DEFAULT NULL);
+
